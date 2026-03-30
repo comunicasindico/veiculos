dd8b4132b2a6f9a36c3bd5d81a3f6bf3a2af0a51
comunicasindico <auditorambientalperito@gmail.com>
2026-03-30 13:40:56 -0300

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+/* ====================================================
+001 – SUPABASE CONFIG
+==================================================== */
+window.SUPABASE_URL="https://gspdlrenyrzbkxlvyugc.supabase.co"
+window.SUPABASE_KEY="sb_publishable_MQRr8O1saGmGz3nsMg1g-Q_8ZIK6yrw"
+window.db=window.supabase.createClient(window.SUPABASE_URL,window.SUPABASE_KEY)
