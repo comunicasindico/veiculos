5fb9575ba109eaf4d15840ffd8d18bda07a7fcc1
comunicasindico <auditorambientalperito@gmail.com>
2026-04-09 23:30:36 -0300

    Fix localStorage setItem order in login.js

--- js/login.js
+++ js/login.js
@@ -23,8 +23,8 @@ if(msg)msg.innerText="Senha incorreta"
 return
 }
 /* LOGIN OK */
-localStorage.setItem("empresa_id",data.empresa_id)
 localStorage.setItem("usuario_id",data.id)
+localStorage.setItem("empresa_id",data.empresa_id)
 localStorage.setItem("usuario_nome",data.nome||"")
 localStorage.setItem("tipo_usuario",data.perfil==="admin"?"admin":"motorista")
 localStorage.setItem("primeiro_login",data.primeiro_login?"1":"0")
