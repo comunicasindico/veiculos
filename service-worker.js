36ce8ed77e65753ff935808003e215babbc5b24c
comunicasindico <auditorambientalperito@gmail.com>
2026-04-29 10:47:22 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -29,11 +29,7 @@ caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS))
 self.addEventListener("activate",e=>{
 e.waitUntil(
 caches.keys().then(keys=>{
-return Promise.all(
-keys.map(k=>{
-if(k!==CACHE_NAME)return caches.delete(k)
-})
-)
+return Promise.all(keys.map(k=>caches.delete(k)))
 })
 )
 self.clients.claim()
